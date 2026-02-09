0bce6f2958e0d979e5e8232a96a598940e7933fd
PinHao Chen <pinhaocrypto@gmail.com>
2026-02-09 10:57:06 +0800

    Updated NTRU+ KPQC version

---
+++ ntruplus-KpqC-Final/Reference_Implementation/NTRU+768/symmetric.c
@@ -0,0 +1,39 @@
+#include <string.h>
+#include "symmetric.h"
+#include "fips202/fips202.h"
+
+#define HASH_F_INBYTES  (NTRUPLUS_POLYBYTES)
+#define HASH_F_OUTBYTES (32)
+
+#define HASH_G_INBYTES  (NTRUPLUS_POLYBYTES)
+#define HASH_G_OUTBYTES (NTRUPLUS_N / 4)
+
+#define HASH_H_INBYTES  (NTRUPLUS_N / 8 + NTRUPLUS_SYMBYTES)
+#define HASH_H_OUTBYTES (NTRUPLUS_SSBYTES + NTRUPLUS_N / 4)
+
+void hash_f(uint8_t *buf, const uint8_t *msg)
+{
+    uint8_t data[1 + HASH_F_INBYTES];
+
+    data[0] = 0x00;
+    memcpy(data + 1, msg, HASH_F_INBYTES);
+    shake256(buf, HASH_F_OUTBYTES, data, HASH_F_INBYTES + 1);
+}
+
+void hash_g(uint8_t *buf, const uint8_t *msg)
+{
+    uint8_t data[1 + HASH_G_INBYTES];
+
+    data[0] = 0x01;
+    memcpy(data + 1, msg, HASH_G_INBYTES);
+    shake256(buf, HASH_G_OUTBYTES, data, HASH_G_INBYTES + 1);
+}
+
+void hash_h(uint8_t *buf, const uint8_t *msg)
+{
+    uint8_t data[1 + HASH_H_INBYTES];
+
+    data[0] = 0x02;
+    memcpy(data + 1, msg, HASH_H_INBYTES);
+    shake256(buf, HASH_H_OUTBYTES, data, HASH_H_INBYTES + 1);
+}
