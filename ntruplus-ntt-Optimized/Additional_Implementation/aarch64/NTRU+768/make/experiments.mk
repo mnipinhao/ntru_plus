051aa1530a4ef3db2889ec74872606bc09811d97
PinHao Chen <pinhaocrypto@gmail.com>
2026-07-13 13:11:29 +0800

    Split GT build targets by responsibility

---
+++ ntruplus-ntt-Optimized/Additional_Implementation/aarch64/NTRU+768/make/experiments.mk
@@ -0,0 +1,45 @@
+.PHONY: test_u01v3_g1_fullpath test_u01v3_g1_reduction_slothy
+.PHONY: test_invntt_lazy_twiddle1_stage123 test_invntt_boundary_experiments
+.PHONY: bench_invntt_next_wave_pmu
+.PHONY: test_kem_invntt_lazy_twiddle1_stage123 test_kem_invntt_lazy_twiddle1_stage123_len16
+.PHONY: profile_kem_gt_production_opt profile_kem_gt_production_opt_rminus1
+
+test_u01v3_g1_fullpath: $(HEADERS) ntt.h gt_test/test_u01v3_g1_fullpath.c $(GT_NTT_ASM_PHASE123_N1) $(U01V3_G1_FULLPATH_ASM) $(U01V3_G1_S2_FULLPATH_ASM) $(U01V3_G1_ABI_SENTINEL_ASM) $(U01V3_G1_S2_ABI_SENTINEL_ASM) asm/gt/experiment/u01_block_first_tables.inc
+	@mkdir -p $(BUILD_DIR)
+	$(CC) $(CPPFLAGS) $(CFLAGS) -std=c99 -Wno-unused-function -I. -o $(BUILD_DIR)/test_u01v3_g1_fullpath gt_test/test_u01v3_g1_fullpath.c $(GT_NTT_ASM_PHASE123_N1) $(U01V3_G1_FULLPATH_ASM) $(U01V3_G1_S2_FULLPATH_ASM) $(U01V3_G1_ABI_SENTINEL_ASM) $(U01V3_G1_S2_ABI_SENTINEL_ASM)
+	./$(BUILD_DIR)/test_u01v3_g1_fullpath
+
+test_u01v3_g1_reduction_slothy: $(HEADERS) ntt.h gt_test/test_u01v3_g1_reduction_slothy.c $(GT_NTT_ASM_PHASE123_N1) $(U01V3_G1_FULLPATH_ASM) $(U01V3_G1_R123_FULLPATH_ASM) $(U01V3_G1_R123_S2_FULLPATH_ASM) $(U01V3_G1_R123_ABI_SENTINEL_ASM) $(U01V3_G1_R123_S2_ABI_SENTINEL_ASM) asm/gt/experiment/u01_block_first_tables.inc
+	@mkdir -p $(BUILD_DIR)
+	$(CC) $(CPPFLAGS) $(CFLAGS) -std=c99 -Wno-unused-function -I. -o $(BUILD_DIR)/test_u01v3_g1_reduction_slothy gt_test/test_u01v3_g1_reduction_slothy.c $(GT_NTT_ASM_PHASE123_N1) $(U01V3_G1_FULLPATH_ASM) $(U01V3_G1_R123_FULLPATH_ASM) $(U01V3_G1_R123_S2_FULLPATH_ASM) $(U01V3_G1_R123_ABI_SENTINEL_ASM) $(U01V3_G1_R123_S2_ABI_SENTINEL_ASM)
+	./$(BUILD_DIR)/test_u01v3_g1_reduction_slothy
+
+test_invntt_lazy_twiddle1_stage123: $(HEADERS) ntt.h ntt.c gt_test/test_invntt_lazy_twiddle1_stage123.c $(GT_NTT_ASM_PHASE123_N1) $(GT_BASE_RMINUS1_OPT_ASM) $(GT_INVNTT_RMINUS1_BASELINE_ASM) $(GT_INVNTT_LAZY_TWIDDLE1_STAGE123_ASM) $(GT_INVNTT_POST_BRANCHFOLD_SLOTHY_ASM)
+	@mkdir -p $(BUILD_DIR)
+	$(CC) $(CPPFLAGS) $(CFLAGS) -std=c99 -Wno-unused-function -I. -o $(BUILD_DIR)/test_invntt_lazy_twiddle1_stage123 gt_test/test_invntt_lazy_twiddle1_stage123.c ntt.c $(GT_NTT_ASM_PHASE123_N1) $(GT_BASE_RMINUS1_OPT_ASM) $(GT_INVNTT_RMINUS1_BASELINE_ASM) $(GT_INVNTT_LAZY_TWIDDLE1_STAGE123_ASM) $(GT_INVNTT_POST_BRANCHFOLD_SLOTHY_ASM)
+	./$(BUILD_DIR)/test_invntt_lazy_twiddle1_stage123
+
+test_invntt_boundary_experiments: $(HEADERS) ntt.h ntt.c gt_test/test_invntt_boundary_experiments.c $(GT_NTT_ASM_PHASE123_N1) $(GT_BASE_RMINUS1_OPT_ASM) asm/gt/experiment/invntt_boundary_experiments.S
+	@mkdir -p $(BUILD_DIR)
+	$(CC) $(CPPFLAGS) $(CFLAGS) -std=c11 -Wno-unused-function -I. -o $(BUILD_DIR)/test_invntt_boundary_experiments gt_test/test_invntt_boundary_experiments.c ntt.c $(GT_NTT_ASM_PHASE123_N1) $(GT_BASE_RMINUS1_OPT_ASM) asm/gt/experiment/invntt_boundary_experiments.S
+	./$(BUILD_DIR)/test_invntt_boundary_experiments
+
+bench_invntt_next_wave_pmu: $(HEADERS) ntt.h ntt.c gt_test/bench_invntt_next_wave_pmu.c $(GT_NTT_ASM_PHASE123_N1) $(GT_BASE_RMINUS1_OPT_ASM) $(GT_INVNTT_RMINUS1_PRODUCTION_ASM) asm/gt/experiment/poly_invntt_rminus1_baseline_namespaced.S $(GT_INVNTT_LAZY_TWIDDLE1_STAGE123_ASM) $(GT_INVNTT_POST_BRANCHFOLD_SLOTHY_ASM) asm/gt/experiment/invntt_boundary_experiments.S
+	@mkdir -p $(BUILD_DIR)
+	$(CC) $(CPPFLAGS) $(CFLAGS) -std=c11 -Wno-unused-function -I. -o $(BUILD_DIR)/bench_invntt_next_wave_pmu gt_test/bench_invntt_next_wave_pmu.c ntt.c $(GT_NTT_ASM_PHASE123_N1) $(GT_BASE_RMINUS1_OPT_ASM) $(GT_INVNTT_RMINUS1_PRODUCTION_ASM) asm/gt/experiment/poly_invntt_rminus1_baseline_namespaced.S $(GT_INVNTT_LAZY_TWIDDLE1_STAGE123_ASM) $(GT_INVNTT_POST_BRANCHFOLD_SLOTHY_ASM) asm/gt/experiment/invntt_boundary_experiments.S
+
+test_kem_invntt_lazy_twiddle1_stage123: $(HEADERS) $(SHAKE_HEADERS) randombytes.h randombytes.c test/test.c ntt.h $(GT_INVNTT_LAZY_TWIDDLE1_KEM_SOURCES)
+	@mkdir -p $(BUILD_DIR)
+	$(CC) $(CPPFLAGS) $(CFLAGS) -std=c99 $(GT_PRODUCTION_DEFAULT_FLAGS) -Wno-unused-function -I. -o $(BUILD_DIR)/test_kem_invntt_lazy_twiddle1_stage123 randombytes.c test/test.c $(GT_INVNTT_LAZY_TWIDDLE1_KEM_SOURCES)
+	./$(BUILD_DIR)/test_kem_invntt_lazy_twiddle1_stage123
+
+test_kem_invntt_lazy_twiddle1_stage123_len16: $(HEADERS) $(SHAKE_HEADERS) randombytes.h randombytes.c test/test.c ntt.h $(GT_INVNTT_LAZY_TWIDDLE1_LEN16_KEM_SOURCES)
+	@mkdir -p $(BUILD_DIR)
+	$(CC) $(CPPFLAGS) $(CFLAGS) -std=c99 $(GT_PRODUCTION_DEFAULT_FLAGS) -Wno-unused-function -I. -o $(BUILD_DIR)/test_kem_invntt_lazy_twiddle1_stage123_len16 randombytes.c test/test.c $(GT_INVNTT_LAZY_TWIDDLE1_LEN16_KEM_SOURCES)
+	./$(BUILD_DIR)/test_kem_invntt_lazy_twiddle1_stage123_len16
+
+profile_kem_gt_production_opt: $(HEADERS) $(SHAKE_HEADERS) randombytes.h randombytes.c gt_test/kem_component_profiler.c ntt.h $(GT_PRODUCTION_DEFAULT_KEM_SOURCES) $(GT_RMINUS1_STAGE123SCRATCH_BENCH_ASM)
+	@mkdir -p $(BUILD_DIR)
+	$(CC) $(CPPFLAGS) $(CFLAGS) -std=c99 $(GT_PRODUCTION_DEFAULT_FLAGS) -DKEM_COMPONENT_PROFILE_TARGET=\"gt_production_default\" -Wno-unused-function -I. -o $(BUILD_DIR)/profile_kem_gt_production_opt randombytes.c gt_test/kem_component_profiler.c $(GT_PRODUCTION_DEFAULT_KEM_SOURCES) $(GT_RMINUS1_STAGE123SCRATCH_BENCH_ASM)
+
+profile_kem_gt_production_opt_rminus1: profile_kem_gt_production_opt
---
+++ ntruplus-ntt-Optimized/Additional_Implementation/aarch64/NTRU+768/make/tests.mk
@@ -0,0 +1,39 @@
+.PHONY: test_gt_baseinv_batch test_gt_basemul_opt
+.PHONY: test_gt_basemul_add32_ref test_gt_basemul_add_direct32_model
+.PHONY: test_gt_basemul_add32_asm test_gt_basemul_add32_full_pipeline_inline
+.PHONY: test_invntt_production_abi
+
+test_gt_baseinv_batch: $(HEADERS) ntt.h ntt.c $(GT_BASE_REF_C) $(GT_BASEINV_BATCH_C) gt_test/test_gt_baseinv_batch.c
+	@mkdir -p $(BUILD_DIR)
+	$(CC) $(CPPFLAGS) $(CFLAGS) -std=c99 -DGT_BASE_REF_NO_ABI_WRAPPERS -DGT_BASEINV_BATCH_NO_ABI_WRAPPER -Wno-unused-function -I. -o $(BUILD_DIR)/test_gt_baseinv_batch gt_test/test_gt_baseinv_batch.c ntt.c $(GT_BASE_REF_C) $(GT_BASEINV_BATCH_C)
+	./$(BUILD_DIR)/test_gt_baseinv_batch
+
+test_gt_basemul_opt: $(HEADERS) ntt.h ntt.c $(GT_BASE_REF_C) $(GT_BASE_OPT_NOADD_ASM) gt_test/test_gt_basemul_asm.c
+	@mkdir -p $(BUILD_DIR)
+	$(CC) $(CPPFLAGS) $(CFLAGS) -std=c99 -DGT_BASE_REF_NO_ABI_WRAPPERS -Wno-unused-function -I. -o $(BUILD_DIR)/test_gt_basemul_opt gt_test/test_gt_basemul_asm.c ntt.c $(GT_BASE_REF_C) $(GT_BASE_OPT_NOADD_ASM)
+	./$(BUILD_DIR)/test_gt_basemul_opt
+
+test_gt_basemul_add32_ref: $(HEADERS) ntt.h ntt.c gt_test/test_gt_basemul_add32_ref.c
+	@mkdir -p $(BUILD_DIR)
+	$(CC) $(CPPFLAGS) $(CFLAGS) -std=c99 -Wno-unused-function -I. -o $(BUILD_DIR)/test_gt_basemul_add32_ref gt_test/test_gt_basemul_add32_ref.c ntt.c
+	./$(BUILD_DIR)/test_gt_basemul_add32_ref
+
+test_gt_basemul_add_direct32_model: $(HEADERS) ntt.h ntt.c gt_test/test_gt_basemul_add_direct32_model.c
+	@mkdir -p $(BUILD_DIR)
+	$(CC) $(CPPFLAGS) $(CFLAGS) -std=c99 -Wno-unused-function -I. -o $(BUILD_DIR)/test_gt_basemul_add_direct32_model gt_test/test_gt_basemul_add_direct32_model.c ntt.c
+	./$(BUILD_DIR)/test_gt_basemul_add_direct32_model
+
+test_gt_basemul_add32_asm: $(HEADERS) ntt.h ntt.c $(GT_BASE_REF_C) $(GT_BASE_ADD32_ASM) gt_test/test_gt_basemul_add32_asm.c
+	@mkdir -p $(BUILD_DIR)
+	$(CC) $(CPPFLAGS) $(CFLAGS) -std=c99 -DGT_BASE_REF_NO_ABI_WRAPPERS -Wno-unused-function -I. -o $(BUILD_DIR)/test_gt_basemul_add32_asm gt_test/test_gt_basemul_add32_asm.c ntt.c $(GT_BASE_REF_C) $(GT_BASE_ADD32_ASM)
+	./$(BUILD_DIR)/test_gt_basemul_add32_asm
+
+test_gt_basemul_add32_full_pipeline_inline: $(HEADERS) ntt.h ntt.c $(GT_BASE_REF_C) $(GT_BASE_ADD32_FULL_PIPELINE_INLINE_ASM) gt_test/test_gt_basemul_add32_asm.c
+	@mkdir -p $(BUILD_DIR)
+	$(CC) $(CPPFLAGS) $(CFLAGS) -std=c99 -DGT_BASE_REF_NO_ABI_WRAPPERS -Wno-unused-function -I. -o $(BUILD_DIR)/test_gt_basemul_add32_full_pipeline_inline gt_test/test_gt_basemul_add32_asm.c ntt.c $(GT_BASE_REF_C) $(GT_BASE_ADD32_FULL_PIPELINE_INLINE_ASM)
+	./$(BUILD_DIR)/test_gt_basemul_add32_full_pipeline_inline
+
+test_invntt_production_abi: $(HEADERS) ntt.h ntt.c gt_test/test_invntt_production_abi.c $(GT_NTT_ASM_PHASE123_N1) $(GT_BASE_RMINUS1_OPT_ASM) $(GT_INVNTT_RMINUS1_PRODUCTION_ASM) $(GT_INVNTT_LAZY_TWIDDLE1_STAGE123_ASM) $(GT_INVNTT_POST_BRANCHFOLD_SLOTHY_ASM)
+	@mkdir -p $(BUILD_DIR)
+	$(CC) $(CPPFLAGS) $(CFLAGS) -std=c99 -Wno-unused-function -I. -o $(BUILD_DIR)/test_invntt_production_abi gt_test/test_invntt_production_abi.c ntt.c $(GT_NTT_ASM_PHASE123_N1) $(GT_BASE_RMINUS1_OPT_ASM) $(GT_INVNTT_RMINUS1_PRODUCTION_ASM) $(GT_INVNTT_LAZY_TWIDDLE1_STAGE123_ASM) $(GT_INVNTT_POST_BRANCHFOLD_SLOTHY_ASM)
+	./$(BUILD_DIR)/test_invntt_production_abi
