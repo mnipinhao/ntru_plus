95f0cacbc6efecc694de7742da964bf97b64a173
PinHao Chen <pinhaocrypto@gmail.com>
2026-07-22 12:48:19 +0800

    Add AArch64 GT transform contract audit

---
+++ ntruplus-ntt-Optimized/Additional_Implementation/aarch64/NTRU+768/make/tests/transform-contract.mk
@@ -0,0 +1,16 @@
+.PHONY: build_gt_transform_audit audit_gt_transform_contract
+
+build_gt_transform_audit: $(HEADERS) ntt.h ntt.c $(GT_BASE_REF_C) \
+		gt_test/test_gt_transform_audit.c gt_test/gt_transform_abi_sentinel.S \
+		$(GT_NTT_ASM) $(GT_INVNTT_ASM) $(GT_INVNTT_RMINUS1_ASM) \
+		$(GT_BASE_OPT_NOADD_ASM) $(GT_BASE_RMINUS1_OPT_ASM)
+	@mkdir -p $(BUILD_DIR)
+	$(CC) $(CPPFLAGS) $(CFLAGS) -std=c99 -DGT_BASE_REF_NO_ABI_WRAPPERS \
+		-Wno-unused-function -I. -o $(BUILD_DIR)/test_gt_transform_audit \
+		gt_test/test_gt_transform_audit.c gt_test/gt_transform_abi_sentinel.S \
+		ntt.c $(GT_BASE_REF_C) $(GT_NTT_ASM) $(GT_INVNTT_ASM) \
+		$(GT_INVNTT_RMINUS1_ASM) $(GT_BASE_OPT_NOADD_ASM) \
+		$(GT_BASE_RMINUS1_OPT_ASM)
+
+audit_gt_transform_contract: build_gt_transform_audit
+	./$(BUILD_DIR)/test_gt_transform_audit
